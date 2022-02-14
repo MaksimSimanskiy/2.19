2d2bd4e6df5ad757bd75fb0aabee6d63eb3172fd
MaksimSimanskiy <78483044+MaksimSimanskiy@users.noreply.github.com>
2022-02-14 11:28:13 +0300

    Update individual2.py

--- individual2.py
+++ individual2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Разработайте аналог утилиты tree в Linux. Используйте возможности модуля argparse для
+
+"""
+Разработайте аналог утилиты tree в Linux. Используйте возможности модуля argparse для
 управления отображением дерева каталогов файловой системы. Добавьте дополнительные
 уникальные возможности в данный программный продукт.
 """
@@ -18,10 +20,9 @@ def tree(directory):
     spacer = ' ' * depth
     print(f'{spacer}+ {path.name}')
 
+    
 def main(command_line=None):
-
     # Создать основной парсер командной строки.
-
     parser = argparse.ArgumentParser()
     parser.add_argument('tree', action="store", help='Main command')
     parser.add_argument('-c', '--command', action="store", help='additional command')
@@ -39,5 +40,6 @@ def main(command_line=None):
         time, file_path = max((f.stat().st_mtime, f) for f in pathlib.Path.cwd().iterdir())
         print(datetime.fromtimestamp(time), file_path)
 
+        
 if __name__ == '__main__':
     main()
